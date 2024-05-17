bcaa768320e6ca67f45278a528ebaad0672f2497
dimkasss <spharde.god@gmail.com>
2024-05-17 17:17:20 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-// import CustomChart from "@/components/CustomChart";
+import CustomChart from "@/components/CustomChart";
 
 export default function Home() {
   return (
     <div>
       <div>Main page (upd)</div>
-      {/* <CustomChart /> */}
+      <CustomChart />
     </div>
   );
 }
