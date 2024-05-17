46f88195db0100327401c8c6f245e46171521f8c
dimkasss <spharde.god@gmail.com>
2024-05-17 16:26:20 +0300

    check if remote updates after push

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import CustomChart from "@/components/CustomChart";
 export default function Home() {
   return (
     <div>
-      <div>Main page</div>
+      <div>Main page (upd)</div>
       <CustomChart />
     </div>
   );
